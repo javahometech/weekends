1ff779e0c8dae94cd7cad2d62a5392d647e1c68c
javahome <kammana.hari@gmail.com>
2016-04-30 20:17:49 +0530

    Java Examples

--- src/MultiThreadDemo.java
+++
@@ -1,9 +0,0 @@
-
-public class MultiThreadDemo {
-	public static void main(String[] args) {
-		OddThread t1 = new OddThread();
-		EvenThread t2 = new EvenThread();
-		t1.start();
-		t2.start();
-	}
-}
